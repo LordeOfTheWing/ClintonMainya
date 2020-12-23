603846796055b7c8de57d2dce2455937dd392efb
WekaMbali <klyntonmainya@gmail.com>
2020-12-23 19:07:01 +0300

    React routing

--- myportifolio/src/App.js
+++ myportifolio/src/App.js
@@ -4,12 +4,31 @@ import GlobalStyle from './components/GlobalStyle';
 
 //Importing Pages
 import AboutMe from '../src/pages/AboutMe';
+import Nav from './components/Nav';
+import ContactMe from '../src/pages/ContactMe';
+import MyWork from '../src/pages/MyWork';
+//Router
+import {Switch, Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
         <GlobalStyle/>
-        <AboutMe/>
+        <Nav />
+
+        <Route path="/">
+          <AboutMe/>
+        </Route>
+
+        <Route path="/mywork">
+          <MyWork/>
+        </Route>
+
+        <Route path="/contactme">
+          <ContactMe/>
+        </Route>
+        
     </div>
   );
 }
--- myportifolio/src/components/FaqSection.js
+++ myportifolio/src/components/FaqSection.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+import {About} from '../Styles';
+
+
 
 
 const FaqSection = () => {
     return(
-        <div className="faq">
+        <Faq>
             <h2>Any Questions ?<span>FAQ</span></h2>
             <div className="question">
                 <h4>What do you specifically do?</h4>
                 <div className="answer">
                     <p>I am a web developer and also a graphic designer</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>What are the languages and frameworks you code in?</h4>
@@ -19,6 +24,7 @@ const FaqSection = () => {
                         I love using React for creation of beautiful front-ends like this one.
                     </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Are you good with back-end?</h4>
@@ -26,6 +32,7 @@ const FaqSection = () => {
                     <p>My dream goal is to be full-stack developer</p>
                     <p>At the moment, I am starting to learn about node js and Express js</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Do you love what you do?</h4>
@@ -33,9 +40,37 @@ const FaqSection = () => {
                     <p>Absolutely yes.</p>
                     <p>I love coming up with solutions and solving complex problems.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-        </div>
+        </Faq>
     )
 };
 
+const Faq = styled(About)`
+    display: block;
+    span{
+        display:block;
+
+    }
+    h2{
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .faq-line{
+        background: #cccccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+    }
+    .question{
+        padding:3rem 0rem;
+        cursor: pointer;
+    }
+    .answer{
+        padding: 2rem 0rem;
+        p{
+            padding: 1rem 0rem;
+        }
+    }
+`;
 export default FaqSection;
